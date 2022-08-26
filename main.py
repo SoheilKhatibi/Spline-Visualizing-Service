b79fde3783db1980519868bf85c08a1132681b11
Soheil Khatibi <soheilkhatibi1377@gmail.com>
2022-08-26 08:44:23 +0430

    Add image input forms and functions

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import os
+
+UPLOAD_FOLDER = './static'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 flaskApp = Flask(__name__)
+flaskApp.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@flaskApp.route('/')
-def hello_world():
-    return 'Hello World!'
+@flaskApp.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template("index.html")
+
+@flaskApp.route('/setInputs', methods=['POST', 'GET'])
+def setInputs():
+    """
+    Set inputs of the algorithm
+    """
+    if request.method == "POST":
+        image = request.files["image"]
+        if not image:
+            return 'No image was uploaded', 400
+        else:
+            image.save(os.path.join(flaskApp.config['UPLOAD_FOLDER'], image.filename))
+            return render_template("setInputs.html", user_image=image.filename)
+
 
 if __name__ == '__main__':
     flaskApp.run()
