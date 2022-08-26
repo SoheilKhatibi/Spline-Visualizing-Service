35863145d3148ae136885f62ddad0ebe4449f707
Soheil Khatibi <soheilkhatibi1377@gmail.com>
2022-08-26 09:52:10 +0430

    Add result.html form

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template
 import os
+import inputParser
+from splineDrawer import splineDrawer
 
 UPLOAD_FOLDER = './static'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
@@ -24,6 +26,29 @@ def setInputs():
             image.save(os.path.join(flaskApp.config['UPLOAD_FOLDER'], image.filename))
             return render_template("setInputs.html", user_image=image.filename)
 
+@flaskApp.route('/infer', methods=['POST', 'GET'])
+def infer():
+    """
+    """
+
+    t = request.form['T']
+    c = request.form['C']
+    k = request.form['K']
+    fileName = request.form['name'][:-1]
+
+    t = inputParser.parseTValues(t)
+
+    c = inputParser.parseCValues(c)
+
+    k = inputParser.parseKValue(k)
+
+    imagePath = os.path.join(flaskApp.config['UPLOAD_FOLDER'], fileName)
+
+    drawer = splineDrawer(imagePath, t, c, k)
+
+    drawer.drawSpline(flaskApp.config['UPLOAD_FOLDER'])
+
+    return render_template('result.html', user_image='result.png')
 
 if __name__ == '__main__':
     flaskApp.run()
--- splineDrawer.py
+++ splineDrawer.py
@@ -1,6 +1,7 @@
 from scipy import interpolate
 from matplotlib import pyplot as plt
 import numpy as np
+import os
 
 class splineDrawer():
     def __init__(self, imgPath, t, c, k):
@@ -9,8 +10,9 @@ class splineDrawer():
         self.c = c
         self.k = k
 
-    def drawSpline(self):
+    def drawSpline(self, resultDir):
         img = plt.imread(self.imagePath)
+        print(img.shape)
         fig, ax = plt.subplots()
         imgPlot = ax.imshow(img)
         spl = interpolate.BSpline(self.t, self.c, self.k)
@@ -18,4 +20,4 @@ class splineDrawer():
         ax.plot(xx, spl(xx), 'b-', lw=4, alpha=0.7, label='BSpline')
         ax.grid(True)
         ax.legend(loc='best')
-        plt.show()
+        plt.savefig(os.path.join(resultDir, 'result.png'), bbox_inches='tight', pad_inches=0, dpi=300)
