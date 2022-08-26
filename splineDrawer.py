0291151dd0ea36c4a717fb3b954943872ce968e3
Soheil Khatibi <soheilkhatibi1377@gmail.com>
2022-08-26 06:33:43 +0430

    Update splinedrawer constructor and drawing function.

--- splineDrawer.py
+++ splineDrawer.py
@@ -1,7 +1,21 @@
 from scipy import interpolate
+from matplotlib import pyplot as plt
+import numpy as np
 
 class splineDrawer():
-    def __init__(img=None):
-        self.image = img
+    def __init__(self, imgPath, t, c, k):
+        self.imagePath = imgPath
+        self.t = t
+        self.c = c
+        self.k = k
 
-    def drawSpline():
+    def drawSpline(self):
+        img = plt.imread(self.imagePath)
+        fig, ax = plt.subplots()
+        imgPlot = ax.imshow(img)
+        spl = interpolate.BSpline(self.t, self.c, self.k)
+        xx = [i for i in range(0, img.shape[1], 50)]
+        ax.plot(xx, spl(xx), 'b-', lw=4, alpha=0.7, label='BSpline')
+        ax.grid(True)
+        ax.legend(loc='best')
+        plt.show()
